e7c772db5e080afe5a370945e9b30fee50fe2e7b
fotini <f.batzoni@gmail.com>
2022-10-05 16:59:43 +0300

    Page Routes and Views

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const { application } = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'jade');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res){
-    res.send('Helloooooooooooooooo')
+    res.render('index');
 })
+
+app.get('/about',function(req,res){
+    res.render('about');
+})
+
 
 app.listen(3000);
 console.log('Server is running on port 3000 ...')
