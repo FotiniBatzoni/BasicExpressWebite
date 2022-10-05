bb41b9b470d6cf8717f285a5172a0b455552b103
fotini <f.batzoni@gmail.com>
2022-10-05 15:47:22 +0300

    Express setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/',function(req,res){
+    res.send('Helloooooooooooooooo')
+})
+
+app.listen(3000);
+console.log('Server is running on port 3000 ...')
